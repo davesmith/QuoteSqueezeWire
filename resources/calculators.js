5c6845f8a420a1d585acd375a17c982b1fa4cfdc
Dave <davesmithdev@gmail.com>
2014-03-31 01:19:10 +0530

    changed...added quotestreet repo

---
+++ resources/calculators.js
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Student Loan Hero - 404</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
+    <style>
+      body {
+        padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+      }
+    </style>
+
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="/images/favicon.ico">
+  </head>
+
+  <body class="noise">
+
+
+    <div class="container-fluid">
+
+		<div class="row-fluid">
+		    <div class="span2">&nbsp;</div>
+		 	 <div class="span8 topspace center error-page well white inverse">
+		<div class="logo"><a href="https://secure.studentloanhero.com"><img src="/assets/images/loan-hero.png"></a>
+		  <h1 class="blue">The page you were looking for doesn't exist.</h1>
+			<p class="large">You may have mistyped the address or the page may have moved.</p>
+		  <a class="btn btn-primary large" href="https://secure.studentloanhero.com/">Beam me up scotty!</a>
+		<div class="clear"></div>
+
+	</div> <!-- / span4 -->
+	    <div class="span2">&nbsp;</div>
+	  </div>
+
+    </div> <!-- /container -->
+
+  </body>
+</html>
